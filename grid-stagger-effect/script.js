ca2938a608e4ae7da8596af881ce48124506e6d3
WellsSA <wel.cavzod@gmail.com>
2023-01-20 16:32:48 -0300

    feat(grid-stagger): using anime.js

--- grid-stagger-effect/script.js
+++ grid-stagger-effect/script.js
@@ -1,15 +1,44 @@
 const TILE_SIZE = 50; // 50px X 50px
 const wrapper = document.getElementById('tiles');
 
-const createTile = index => {
+const colors = [
+  'rgba(229, 57,53)',
+  'rgba(253, 216, 53)',
+  'rgba(244, 81,30)',
+  'rgba(76, 175, 80)',
+  'rgba(33, 150, 243)',
+  'rgba(156, 39, 176)',
+];
+
+let clickCounter = 0;
+const handleOnClick = (index, columns, rows) => {
+  clickCounter = clickCounter + 1;
+
+  const tileColor = colors[clickCounter % (colors.length - 1)];
+  console.log({ anime });
+  anime({
+    targets: '.tile',
+    easing: 'easeOutExpo',
+    backgroundColor: tileColor,
+    delay: anime.stagger(50, {
+      grid: [columns, rows],
+      from: index,
+    }),
+  });
+};
+
+const createTile = (index, onClick) => {
   const tile = document.createElement('div');
   tile.classList.add('tile');
+
+  tile.onclick = () => onClick(index);
+
   return tile;
 };
 
-const createTiles = (elementWrapper, quantity) => {
+const createTiles = (elementWrapper, quantity, onClick) => {
   Array.from(Array(quantity)).map((_, index) => {
-    elementWrapper.appendChild(createTile(index));
+    elementWrapper.appendChild(createTile(index, onClick));
   });
 };
 
@@ -22,7 +51,8 @@ const createGrid = (elementWrapper, tileSize) => {
   elementWrapper.style.setProperty('--columns', columns);
   elementWrapper.style.setProperty('--rows', rows);
 
-  createTiles(elementWrapper, columns * rows);
+  const onClick = index => handleOnClick(index, columns, rows);
+  createTiles(elementWrapper, columns * rows, onClick);
   console.log(`Grid created with ${columns} columns and ${rows} rows`);
 };
 
